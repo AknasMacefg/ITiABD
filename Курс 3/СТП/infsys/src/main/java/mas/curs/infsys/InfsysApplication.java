266de30046214ef51de5c151630ba929479a7a51
AknasMacefg <alex.maslov14@gmail.com>
2025-11-20 15:20:05 +0300

    kurs update 1

---
+++ Курс 3/СТП/infsys/src/main/java/mas/curs/infsys/InfsysApplication.java
@@ -0,0 +1,13 @@
+package mas.curs.infsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfsysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfsysApplication.class, args);
+    }
+
+}
---
+++ Курс 3/СТП/infsys/src/test/java/mas/curs/infsys/InfsysApplicationTests.java
@@ -0,0 +1,13 @@
+package mas.curs.infsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfsysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
