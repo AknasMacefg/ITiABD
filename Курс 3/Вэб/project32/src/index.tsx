9a9728e03b95b339a006cea065ef7a4d6a42d454
AknasMacefg <alex.maslov14@gmail.com>
2026-04-16 01:43:52 +0300

    23423
    123

--- Курс 3/Вэб/project32/src/index.tsx
+++ Курс 3/Вэб/project32/src/index.tsx
@@ -1,15 +1,17 @@
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
+let name = "Проша";
+let profession = "frontend-разработчиком";
 const domNode = document.getElementById("root") as HTMLDivElement;
 const root = createRoot(domNode);
 root.render(
   <>
     <p>
-      Привет, меня зовут <span className="name">Проша</span>!
+      Привет, меня зовут <span className="name">{name}</span>!
     </p>
     <p>
-      Скоро я стану крутым <span className="profession">frontend-разработчиком</span>!
+      Скоро я стану крутым <span className="profession">{profession}</span>!
     </p>
   </>
 );
